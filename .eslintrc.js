282c8269e9661c5d27e770f5c28bb72d7ff222d3
Fco Javier Rodríguez Crespo <Fcojavierrodriguezcrespo@gmail.com>
2018-07-08 22:12:16 +0200

    Add files .eslintrc.js, .gitignore and locales(Contains the translations errors)

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+    }
+};
