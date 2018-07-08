4abc7dcddcf50ddb8ec6c23201b941adbed61322
Fco Javier Rodríguez Crespo <Fcojavierrodriguezcrespo@gmail.com>
2018-07-08 05:54:17 +0200

    Add the controlers (user.js, advertisements.js)

--- routes/api/v1.0/advertisements.js
+++ routes/api/v1.0/advertisements.js
@@ -1,1 +1,69 @@
 'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const Advertisement = require('../../../models/Advertisement'); 
+const jwtAuth = require('../../../lib/jwtAuth');
+
+// Get all advertisiments or advertisements filtered
+router.get('/',jwtAuth(), async (req, res, next)=>{
+    try{
+        Advertisement.collection.getIndexes({full: true}).then(indexes => {
+            console.log("indexes:", indexes);
+            
+        }).catch(console.error);
+        // Get the param or params for do the query
+        const name = req.query.name;
+        const tag = req.query.tag;
+        const sale = req.query.sale;
+        const price = req.query.price;
+        const maxprice = req.query.maxprice;
+        const minprice = req.query.minprice;
+        const user = req.query.user;
+        const skip = parseInt(req.query.skip);
+        const limit = parseInt(req.query.limit);
+        const fields = req.query.fields;
+        const sort = req.query.sort; // La ordenación se ejecuta antes que el paginado.
+
+        // Empty filter
+        const filter = {};
+        
+        // The filter is added only when is necesary filter
+        if(name){ 
+            filter.name = new RegExp('^' + name, "i"); //Filter by name
+        }
+
+        if(tag){
+            filter.tags = tag; //Filter by tag
+        }
+        if(sale){
+            filter.sale = sale; //Filter by sale o search
+        }
+
+        if(price){
+            filter.price = price; //Filter by price
+        }
+
+        if(maxprice && minprice){ //Filter price between min price and max price (both included).
+            filter.price = {$gte: minprice, $lte: maxprice};
+        }
+        if(maxprice){ //Filter price less than maxprice included
+            filter.price = {$lte: maxprice};
+        }if(minprice){ //Filter price morte than minprice included
+            filter.price = {$gte: minprice};
+        }
+        
+        if(user){ //Filter by user
+            filter.user = user; 
+        }
+
+        //const agentes = await Agente.list(filter);  // await espera a que se resuelva la promsea y me da el resultado.
+        const advertisements = await Advertisement.list(filter, skip, limit, fields,sort);
+        res.json({success: true, result: advertisements});
+    }catch(err){
+        next(err);
+    }
+});
+
+module.exports = router;
--- routes/api/v1.0/users.js
+++ routes/api/v1.0/users.js
@@ -1,1 +1,95 @@
 'use strict';
+
+/*********************************************************************************************************
+ *                              Middleware/Controller for users                                          *
+ *********************************************************************************************************
+ *                                                                                                       *
+ * This middleware contain all methods for process the users request from the client.                    *
+ *                                                                                                       *
+*********************************************************************************************************/
+
+/*********************************************************************************************************
+ *                                                Constants                                              *
+ *********************************************************************************************************
+ * express - Import framework express. Contain all methods of express.                                   *
+ * router - It's the router for users. It's an isolated instance of middleware and routes.               *
+ *                                                                                                       *
+ *********************************************************************************************************/
+const express = require('express');
+const router = express.Router();
+
+const User = require('../../../models/User'); 
+const encrypt = require('../../../lib/encrypt_decrypt');
+const jwtAuth = require('../../../lib/jwtAuth');
+
+/****************************************** GET Methods **************************************************/
+
+// Get an user by email
+router.get('/:email', async (req, res, next)=>{
+    try{
+        const email = req.params.email;
+        const user = await User.getUser(email);
+
+        res.json({success: true, result: user});
+    }catch(err){
+        next(err);
+    }
+});
+
+// Get user list
+router.get('/',jwtAuth(), async (req, res, next)=>{
+    try{
+        
+        const users = await User.getUsers();
+
+        res.json({success: true, result: users});
+    }catch(err){
+        next(err);
+    }
+});
+
+/****************************************** POST Methods **************************************************/
+
+// Create user
+router.post('/', async (req, res, next) =>{
+
+    try{
+        
+        const cypheredOptions = encrypt.encryptData(req.body.password);
+        // Encrypted password
+        const pass = cypheredOptions.encrypted;
+        // Cyphered key and initialization vector
+        delete cypheredOptions.encrypted;
+        
+        // Register user
+        const registeredUser = await User.register(req.body.email,pass,cypheredOptions);
+        //Response
+        res.json({success: true, result: registeredUser});
+    }catch(err){
+        next(err);
+    }
+});
+
+// Login
+router.post('/login', (req, res, next) =>{
+    try {
+
+        const email = req.body.email;
+        const password = req.body.password;
+        // Check if the user is authenticated, it's true return the token(JWT) else return error.
+        encrypt.authentication(User,email,password).then(function (result){
+            
+            if(result.succes === false){
+                const error = res.__('Error credentials');
+                res.json({success: false, result: error});
+            }
+            res.json({success: true, result: result});
+        });
+        
+    } catch (error) {
+        next(error);
+    }
+});
+
+
+module.exports = router;
