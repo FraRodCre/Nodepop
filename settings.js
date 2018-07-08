075378c1e204c0b4a1c6e19831de3f51beea46a4
Fco Javier Rodríguez Crespo <Fcojavierrodriguezcrespo@gmail.com>
2018-07-08 05:55:13 +0200

    Add file settings.js with the information for generate JWT

---
+++ settings.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    jwt:{
+        secret: '+q/a_X"gGCa{r7:kAMq,&e^{(74t4YHd4H-pZw~p}FappQvP4*733j3{*Y"_4/]@35}H_QMyb#EeGTBmX3N")GuXLS#4)y;xg,DZ',
+        expiresIn: '2d'
+    }
+};
